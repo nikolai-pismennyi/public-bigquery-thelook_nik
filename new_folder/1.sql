f7db685b32f23b4cd0e9fc06db9c887029864164
atad_name <atad_email@email.com>
2024-05-23 14:24:12 +0000

    Edit /public-bigquery-thelook_nik/new_folder/1.sql
    By promt: generate me list of users in SQL

--- new_folder/1.sql
+++ new_folder/1.sql
@@ -1,2 +1,2 @@
 Error: Failed to extract metadata for your project.
-metadata_init_error_log_id: 'fcc4bbe6-5d7d-4b2b-bddf-ee3088d090a6'
+metadata_init_error_log_id: '583a24c7-76d8-4487-9cbe-1b3896afeb38'
